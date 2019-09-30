6a021770c46f1ba689d608fa34d3281949ee1f0f
Alex Li <guohua_lee@163.com>
2019-09-30 22:02:07 +1000

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const axios = require('axios');
+require('dotenv').config()
+const app = express();
+
+app.get('/api/autocomplete/:query', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    const endpoint = `${process.env.ENDPOINT}?app_id=${process.env.APP_ID}&app_code=${process.env.APP_CODE}&query=${req.params.query}&beginHighlight=<b>&endHighlight=</b>&country=AUS&maxresults=5`;
+    axios({
+        method: 'GET',
+        url: endpoint,
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json"
+        }
+      }).then(response => {
+        res.send(JSON.stringify(response.data));
+      }).catch(error => {
+        res.send(JSON.stringify(error), 400);
+      });
+})
+
+app.listen(3001, () => console.log('Express server is running on localhost:3001'));
